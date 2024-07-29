01b948bbfdfe69772693cbce5fc12ee648bde908
vigneshpalani3 <vigneshpalani.dev@gmail.com>
2024-07-29 18:36:36 +0530

    changed to cloud

--- backend/db/db.js
+++ backend/db/db.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose')
 
 async function connectDB(){
   try{
-    await mongoose.connect('mongodb://127.0.0.1:27017/expence-tracker')
-
+    await mongoose.connect(process.env.MONGO_URl,{dbName:'userData'})
     console.log('db connected')
   }catch(err){
     console.log(err)
